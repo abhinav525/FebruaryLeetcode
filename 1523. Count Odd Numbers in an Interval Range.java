3791cd408810c287cff7934b772c0029272b2998
ABHINAV <78312043+abhinav525@users.noreply.github.com>
2023-02-13 22:09:42 +0530

    Create 1523. Count Odd Numbers in an Interval Range.java

---
+++ 1523. Count Odd Numbers in an Interval Range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        
+     if(low%2==0)
+       low = low+1;
+     if(high%2==0)
+        high = high-1;
+    if (high - low == 0) return 1;
+        return ((high - low) / 2) + 1;
+         
+        
+    }
+}
